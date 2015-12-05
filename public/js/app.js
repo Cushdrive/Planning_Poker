1f706fc7b06b2718aa69e465b2a5da663fd449c7
Jayson Bender <jayson@Jaysons-MacBook-Pro.local>
2015-12-05 14:08:08 -0600

    Added countdown for timer.

--- public/js/app.js
+++ public/js/app.js
@@ -26,21 +26,35 @@
 	//Setup a controller so we can bubble menu click information down to the individual controllers
 	var menuController = function ($scope, $interval) {
 		$scope.timeout = "";
+		$scope.timerDisabled = false;
 		var menuOpen = false;
 		var stoptime = "";
+		var intervals = 0;
+		//Update the timer box every 10 seconds.
+		var constCallInterval = 10000;
 
 		var publishtimeout = function () {
-			$interval.cancel(stoptime);
-			$scope.$broadcast('onTimerTimeout',$scope.timeout);
-			$scope.timeout = "";
+			intervals -= 1;
+			if (intervals > 0) {
+				//e.g. (2 * 10,000)/1000 = 20 seconds
+				$scope.timeout = ((intervals * constCallInterval)/1000) + " seconds";
+			}
+			else {
+				$scope.timerDisabled = false;
+				$interval.cancel(stoptime);
+				$scope.$broadcast('onTimerTimeout',$scope.timeout);
+				$scope.timeout = "";
+			}
 		};
 
 		$scope.time = function () {
 			var timeout = Number($scope.timeout);
 			$interval.cancel(stoptime);
 			if (timeout != "NaN") {
+				$scope.timerDisabled = true;
 				timeout = timeout * 60000;
-				stoptime = $interval(publishtimeout,timeout);
+				intervals = timeout/constCallInterval;
+				stoptime = $interval(publishtimeout,constCallInterval);
 			}
 			
 		};
